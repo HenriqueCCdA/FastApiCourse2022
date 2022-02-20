e1b1f1c077c27ae9eae76e8e11cff78d0de30508
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2022-02-20 16:38:03 -0300

    Inicio do curso

--- books.py
+++ books.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 from enum import Enum
 
@@ -21,7 +22,11 @@ class DirectionName(str, Enum):
 
 
 @app.get('/')
-async def read_all_books():
+async def read_all_books(skip_book: Optional[str] = None):
+    if skip_book:
+        new_books = BOOKS.copy()
+        del new_books[skip_book]
+        return new_books
     return BOOKS
 
 
@@ -43,4 +48,31 @@ async def read_favorite_book():
 
 @app.get('/books/{book_id}')
 async def read_book(book_id: int):
-    return {"book_title": book_id}
+    return BOOKS[book_id]
+
+
+@app.post('/')
+async def create_book(book_title, book_author):
+    current_book_id = 0
+
+    if len(BOOKS) > 0:
+        for book in BOOKS:
+            x = int(book.split('_')[-1])
+            if x > current_book_id:
+                current_book_id = x
+
+    BOOKS[f'book_{current_book_id + 1}'] = {'title': book_title, 'author': book_author}
+    return BOOKS[f'book_{current_book_id + 1}']
+
+
+@app.put('/{book_name}')
+async def update_book(book_name: str, book_title: str, book_author: str ):
+    book_information = {'title': book_title, 'author': book_author}
+    BOOKS[book_name] = book_information
+    return book_information
+
+
+@app.delete("/")
+async def delete_book(book_name):
+    del BOOKS[book_name]
+    return f'Book {book_name} deleted'
