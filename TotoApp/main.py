ebbc21bc305b2cbdb23e0639ed50fd0254a20447
Henrique Andrade <henrique.ccda@gmail.com>
2022-02-28 16:13:44 -0300

    Metodo put.

--- TotoApp/main.py
+++ TotoApp/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, Depends
+from typing import Optional
+from fastapi import FastAPI, Depends, HTTPException
 from TotoApp.database import SessionLocal
 import models
 from database import engine
 from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
 
 
 app = FastAPI()
@@ -18,6 +20,65 @@ def get_db():
         db.close()
 
 
+class Todo(BaseModel):
+    title: str
+    description: Optional[str]
+    priority: int = Field(gt=0, lt=6, description="The priority must be between 1-5")
+    complete: bool
+
+
 @app.get("/")
 async def read_all(db: Session = Depends(get_db)):
     return db.query(models.Todos).all()
+
+
+@app.get("/todo/{todo_id}")
+async def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    if todo_model is not None:
+        return todo_model
+
+    raise http_excetion()
+
+
+@app.post("/")
+async def create_todo(todo: Todo, db: Session = Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+
+    db.add(todo_model)
+    db.commit()
+
+    return {
+        'status': 201,
+        'transaction': 'Successful'
+    }
+
+
+@app.put("/{todo_id}")
+async def update_todo(todo_id: int, todo: Todo, db: Session = Depends(get_db)):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+
+    if todo_model is None:
+        raise http_excetion()
+
+    todo_model.title = todo.title
+    todo_model.description = todo.description
+    todo_model.priority = todo.priority
+    todo_model.complete = todo.complete
+
+    db.add(todo_model)
+    db.commit()
+
+    return {
+        'status': 200,
+        'transaction': 'Successful'
+    }
+
+
+def http_excetion():
+    return HTTPException(status_code=404, detail="Todo not found")
