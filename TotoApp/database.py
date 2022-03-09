9cef5705ce5d39820fb23fbc5f2d3ec82c2f5cfc
Henrique Andrade <henrique.ccda@gmail.com>
2022-03-09 10:21:15 -0300

    Seção 14

--- TotoApp/database.py
+++ TotoApp/database.py
@@ -14,7 +14,3 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-
-#   "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJjb25kaW5nd2l0aHJvYnkiLCJpZCI6MSwiZXhwIjoxNjQ2NjY3NjA4fQ.oJ3Z1LaZ4d5Z7UGYqZou5XvOZLbmqXZw9rDUU2CFd-g"
-#   "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJleGFtcGxldXNlciIsImlkIjoyLCJleHAiOjE2NDY2Njk4NzB9.uSDRAnmxOP_UaFTcSfY_c4gw2fn2XiCn1as3C-GqLKY"
